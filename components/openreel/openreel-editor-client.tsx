a9ee8f571cc6e62a701bdf4763792e16ede361ff
Cicentos <cimugabo24@gmail.com>
2026-05-10 04:58:25 -0400

    style: polish editor menus and dialogs

--- components/openreel/openreel-editor-client.tsx
+++ components/openreel/openreel-editor-client.tsx
@@ -6,8 +6,13 @@ import OpenReelApp from '@/vendor/openreel/web/App'
 export function OpenReelEditorClient() {
   useEffect(() => {
     document.documentElement.classList.add('dark')
+    document.body.classList.add('markit-editor-active')
     if (!window.location.hash || window.location.hash === '#/' || window.location.hash === '#/welcome') {
       window.location.hash = '#/editor'
+    }
+
+    return () => {
+      document.body.classList.remove('markit-editor-active')
     }
   }, [])
 
